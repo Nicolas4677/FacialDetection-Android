b10a912361ae358ecbf88252bde5dcc04a02a318
Nicolas Morales Escobar <pg18nicolas@vfs.com>
2020-07-29 21:40:52 -0700

    API Upgrade
    Changed from Mobile Vision API to MLKit Vision API, which includes more features for face detection.

--- FacialRecognitionAnimator/app/src/main/java/com/example/facialrecognitionanimator/MainActivity.kt
+++ FacialRecognitionAnimator/app/src/main/java/com/example/facialrecognitionanimator/MainActivity.kt
@@ -1,17 +1,20 @@
+// Copyright (C) 2020, Nicolas Morales Escobar. All rights reserved.
+
 package com.example.facialrecognitionanimator
 
-import android.app.AlertDialog
 import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.util.SparseArray
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.vision.Frame
-import com.google.android.gms.vision.face.Face
-import com.google.android.gms.vision.face.FaceDetector
+import com.google.android.gms.vision.face.Contour
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceContour
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.face.FaceLandmark
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,72 +27,73 @@ class MainActivity : AppCompatActivity() {
 
             override fun onClick(v : View) {
 
-                val myImageView = findViewById<ImageView>(R.id.imgview)
-                val options = BitmapFactory.Options()
-                options.inMutable = true
-                val myBitmap = BitmapFactory.decodeResource(
+                val bitmapOptions = BitmapFactory.Options();
+                bitmapOptions.inMutable = true
+
+                val imageBitmap = BitmapFactory.decodeResource(
                     applicationContext.resources,
-                    R.drawable.test1,
-                    options
-                )
+                    R.drawable.test3,
+                    bitmapOptions)
 
-                val myRectPaint = Paint()
-                myRectPaint.strokeWidth = 10f
-                myRectPaint.color = Color.RED
-                myRectPaint.style = Paint.Style.STROKE
+                val rectPaint = Paint()
+                rectPaint.strokeWidth = 15f
+                rectPaint.color = Color.RED
+                rectPaint.style = Paint.Style.STROKE
 
-                val tempBitmap = Bitmap.createBitmap(
-                    myBitmap.width,
-                    myBitmap.height,
-                    Bitmap.Config.RGB_565
-                )
+                val canvas = Canvas(imageBitmap)
+                canvas.drawBitmap(imageBitmap, 0f, 0f, null)
 
-                val tempCanvas = Canvas(tempBitmap)
-                tempCanvas.drawBitmap(myBitmap, 0f, 0f, null)
+                var faceDetectorOptions = FaceDetectorOptions.Builder()
+                    .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                    .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                    .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+                    .build()
 
-                //Tracking enabled when using video
-                val faceDetector: FaceDetector =
-                    FaceDetector.Builder(applicationContext)
-                        .setTrackingEnabled(false)
-                        .setLandmarkType(FaceDetector.ALL_LANDMARKS)
-                        .build()
-                if (!faceDetector.isOperational) {
-                    AlertDialog.Builder(v.context).setMessage("Could not set up the face detector!").show()
-                    return
-                }
+                val inputImage = InputImage.fromBitmap(imageBitmap, 0)
 
-                val frame: Frame = Frame.Builder().setBitmap(myBitmap).build()
-                val faces: SparseArray<Face> = faceDetector.detect(frame)
+                val faceDetector = FaceDetection.getClient(faceDetectorOptions)
 
-                for (i in 0 until faces.size()) {
+                val result = faceDetector.process(inputImage)
+                    .addOnSuccessListener { faces ->
 
-                    val face = faces.valueAt(i)
+                        for(face in faces)
+                        {
+                            val bounds = face.boundingBox
+                            val rotY = face.headEulerAngleY
+                            val rotZ = face.headEulerAngleZ
 
-                    var landmarks = face.landmarks
+                            val faceDrawer = FaceDrawer(face, canvas, rectPaint)
 
-                    for(i in 0 until 2)
-                    {
-                        val landmark = landmarks[i];
-                        var position: PointF = landmark.position
-                        tempCanvas.drawCircle(position.x, position.y, 15f, myRectPaint)
+                            faceDrawer.drawCompleteFace()
+
+                            rectPaint.color = Color.BLUE
+
+                            faceDrawer.drawFacialContour(FaceContour.FACE)
+                            faceDrawer.drawFacialContour(FaceContour.LEFT_EYE)
+                            faceDrawer.drawFacialContour(FaceContour.RIGHT_EYE)
+                            faceDrawer.drawFacialContour(FaceContour.LOWER_LIP_BOTTOM)
+                            faceDrawer.drawFacialContour(FaceContour.LOWER_LIP_TOP)
+                            faceDrawer.drawFacialContour(FaceContour.UPPER_LIP_BOTTOM)
+                            faceDrawer.drawFacialContour(FaceContour.UPPER_LIP_TOP)
+                        }
+
+                        val imageView = findViewById<ImageView>(R.id.imgview)
+                        imageView.setImageDrawable(BitmapDrawable(resources, imageBitmap))
                     }
+                    .addOnFailureListener{ e ->
 
-                    val mouthLeft = landmarks[5].position;
-                    val mouthRight = landmarks[6].position;
-                    val mouthMiddle = landmarks[7].position;
-
-                    tempCanvas.drawLine(mouthLeft.x, mouthLeft.y, mouthRight.x, mouthRight.y, myRectPaint)
-                    tempCanvas.drawLine(mouthRight.x, mouthRight.y, mouthMiddle.x, mouthMiddle.y, myRectPaint)
-                    tempCanvas.drawLine(mouthMiddle.x, mouthMiddle.y, mouthLeft.x, mouthLeft.y, myRectPaint)
-                }
-                myImageView.setImageDrawable(BitmapDrawable(resources, tempBitmap))
+                    }
             }
         })
     }
 }
 
 //TODO: Change API to MLKit from https://developers.google.com/ml-kit
+//MLKit Vision API
+//To detect the contours of the face, MLKit requires an image of at least 200x200 Pixels.
+//Input image can be:  Bitmap, media.Image, ByteBuffer, byte array, or a file on the device.
 
+//Mobile Vision API
 //ORDER OF FACIAL LANDMARKS
 // 0 and 1 are the eyes (0 being right eye and 1 being left eye)
 // 2 being the nose
